5b7ee6adf032f4db43356a7a4ef91c286fb50bbe
凌览 <lihai_js@163.com>
2025-08-15 08:36:26 +0800

    refactor(组件): 将右上角固定图标移动到App组件中
    将原本在simple-model组件中的右上角固定图标移动到App组件中，通过插槽方式实现组件解耦

--- src/components/simple-model/index.vue
+++ src/components/simple-model/index.vue
@@ -18,20 +18,7 @@
     />
     <nice-fail v-if="isFail" />
   </div>
-  <!-- 右上角固定图标 -->
-  <div
-    v-show="!isLoading"
-    class="absolute top-4 right-4 flex bg-white px-3 py-1 rounded-full"
-  >
-    <a-space>
-      <a-tooltip content="常驻桌面">
-        <a-button shape="circle" style="--color-secondary: #fff">
-          <icon-pushpin class="!w-5 !h-5" />
-        </a-button>
-      </a-tooltip>
-      <select-model />
-    </a-space>
-  </div>
+  <slot name="default" :isLoading="isLoading"></slot>
 </template>
 <script>
 import LiveDisplay from "./LiveDisplay.vue";
@@ -40,9 +27,7 @@ import NiceFail from "../NiceFail.vue";
 import { ref, computed, onMounted, onUnmounted } from "vue";
 import useModelStore from "../../store/model";
 import { debounce } from "lodash-es";
-import { IconPushpin } from "@arco-design/web-vue/es/icon";
-// import { shallowRef, reactive, toRaw, unref } from "vue";
-import SelectModel from "@components/select-model";
+
 export default {
   name: "simple-model-render",
   install(Vue) {
@@ -51,9 +36,7 @@ export default {
   components: {
     LiveDisplay,
     NiceLoading,
-    NiceFail,
-    IconPushpin,
-    SelectModel,
+    NiceFail
   },
   setup() {
     const getCavSize = () => ({
--- src/home/App.vue
+++ src/home/App.vue
@@ -1,10 +1,36 @@
 <template>
   <div class="h-screen w-screen overflow-hidden" style="background: #f5f5f5">
-    <simple-model-render />
+    <simple-model-render>
+      <template #default="{ isLoading }">
+        <!-- 右上角固定图标 -->
+        <div
+          v-show="!isLoading"
+          class="absolute top-4 right-4 flex bg-white px-3 py-1 rounded-full"
+        >
+          <a-space>
+            <a-tooltip content="常驻桌面">
+              <a-button shape="circle" style="--color-secondary: #fff">
+                <icon-pushpin class="!w-5 !h-5" />
+              </a-button>
+            </a-tooltip>
+            <select-model />
+          </a-space>
+        </div>
+      </template>
+    </simple-model-render>
   </div>
 </template>
 
 <script>
+import SelectModel from "@components/select-model";
+import { IconPushpin } from "@arco-design/web-vue/es/icon";
+export default {
+  components: {
+    SelectModel,
+    IconPushpin,
+  },
+  setup() {},
+};
 </script>
 <style>
 html,
